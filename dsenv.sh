b985e7a549881259b449c1c2bf0293d7c041e692
Anne L'Hôte <anne.lhote@gmail.com>
2018-09-06 10:00:01 +0200

    [fix] Correct default image name

--- dsenv.sh
+++ dsenv.sh
@@ -7,7 +7,7 @@ fi
 
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 export DS_HOME=${DS_HOME:-`pwd`}
-export DS_IMAGE=${DS_IMAGE:-dsdev}
+export DS_IMAGE=${DS_IMAGE:-dsenv}
 
 if [[ $1 == "start" ]]; then 
   docker-compose -f ${CURDIR}/docker-compose.yml -p datashare up -d

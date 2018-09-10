ba0375ce838594ff59bb8c42e7230c46fa228d6a
Bruno Thomas <bruno@barreverte.fr>
2018-09-10 14:40:25 +0000

    [zsh] adds set-option default-shell for tmux

--- enter_dsenv.sh
+++ enter_dsenv.sh
@@ -7,4 +7,10 @@ if [ -z "$PROFILE_SHELL" ]; then
   PROFILE_SHELL=bash
 fi
 
+if [ "$PROFILE_SHELL" == "zsh" ]; then
+  if ! grep -q 'default-shell /bin/zsh' .tmux.conf ; then
+    echo "set-option -g default-shell /bin/zsh" >> .tmux.conf
+  fi
+fi
+
 docker exec -ti datashare_dsenv_1 script -q -c "/sbin/setuser dev /bin/$PROFILE_SHELL -c 'cd ~ && /bin/$PROFILE_SHELL'" /dev/null
